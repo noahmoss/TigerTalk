5f6300c1e8228a88ce381e318afa768063d82936
ryangizzie <37642934+ryangizzie@users.noreply.github.com>
2018-04-04 17:32:19 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,46 @@
+$(document).ready(function() {
+	$("#post1").click(function() {
+  		if ($("#reply1").css("display") == "none") {
+        	$("#reply1").css("display", "block");
+    	}
+    	else {
+        	$("#reply1").css("display", "none");
+    	}
+	});
+
+	$("#post2").click(function() {
+  		if ($("#reply2").css("display") == "none") {
+        	$("#reply2").css("display", "block");
+    	}
+    	else {
+        	$("#reply2").css("display", "none");
+    	}
+	});
+
+	$("#post3").click(function() {
+  		if ($("#reply3").css("display") == "none") {
+        	$("#reply3").css("display", "block");
+    	}
+    	else {
+        	$("#reply3").css("display", "none");
+    	}
+	});
+
+	$("#post4").click(function() {
+  		if ($("#reply4").css("display") == "none") {
+        	$("#reply4").css("display", "block");
+    	}
+    	else {
+        	$("#reply4").css("display", "none");
+    	}
+	});
+
+	$("#post5").click(function() {
+  		if ($("#reply5").css("display") == "none") {
+        	$("#reply5").css("display", "block");
+    	}
+    	else {
+        	$("#reply5").css("display", "none");
+    	}
+	});
+});
