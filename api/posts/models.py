c5d9bbef856e1ba04ef414783944e259ae23902a
Noah Moss <noahbmoss@gmail.com>
2018-04-04 20:29:42 -0400

    created initial post model and admin interface

--- api/posts/models.py
+++ api/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    postID = models.IntegerField()
+    user = models.CharField(max_length=20)
+    text = models.TextField(max_length=5000)
+    timestamp = models.DateTimeField()
+
+    def __str__(self):
+        """A string representation of the post."""
+        return self.text[:50]
