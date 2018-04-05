d2e17fe1342926395fcf3b7da7e4265475589196
Noah Moss <noahbmoss@gmail.com>
2018-04-04 21:31:37 -0400

    updates

--- api/posts/models.py
+++ api/posts/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    postID = models.IntegerField()
     user = models.CharField(max_length=20)
     text = models.TextField(max_length=5000)
     timestamp = models.DateTimeField()
--- api/posts/views.py
+++ api/posts/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from . import models
+from . import serializers
 
 # Create your views here.
+class ListPost(generics.ListCreateAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
+class DetailPost(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
