6dd29d9aebf46855b9cd13652759b1a6033d5317
Noah Moss <noahbmoss@gmail.com>
2018-04-11 20:15:55 -0400

    fixed get request

--- app/save.js
+++ app/save.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
-		var length;
 	    $.ajax({
         type: 'GET',
-        url: 'https://tigertalkapi.herokuapp.com/posts/', 
+        url: 'http://tigertalkapi.herokuapp.com/posts/',
         data: { get_param: 'value' },
         dataType: 'json',
         success: function (data) {
             var out = "";
 			var i;
-			length = data.length;
-
 			for (i = 0; i < data.length; i ++) {
 			out += '<div class="chunk"> <div class="media offset-md-1"> <div class="media-body"> <div class="entry" id="e' + i;
-			out += '">' + arr[i].content + '</div> <div class="comments" id="c' + i + '">';
+			out += '">' + data[i].content + '</div> <div class="comments" id="c' + i + '">';
 			comments = data[i].comments;
 			var j;
 			for (j = 0; j < comments.length; j++) {
@@ -25,35 +22,10 @@ $(document).ready(function() {
 			out += ' </div> </div> </div>';
 			}
 
-            $('#chunks').html(out);
+            $('#chunks').append(out);
         },
         error: function () {
         	window.alert("rip");
         }
     });
-
-	var h;
-
-	for (h = 0; h < length; h++) {
-	    var e = "#e" + i;
-	    var c = "#c" + i;
-	    $(e).click(function() {
-  		if ($(c).css("display") == "none") {
-        	$(c).css("display", "block");
-    	}
-    	else {
-        	$(c).css("display", "none");
-    	}
-	   	});
-	}
-
-	$("#mainpost").click(function() {
-		var post = $("#maintext").val();
-		if (post.length != 0) {
-
-
-		}
-	});
-
-
 });
