b00b2bd9a05497d3f663c97aeec7bc3c8c7d60b4
Devina Singh <devinas@princeton.edu>
2018-04-12 01:57:14 -0400

    frontend changes

--- app/save.js
+++ app/save.js
@@ -3,54 +3,43 @@ $(document).ready(function() {
 	var h;
     $.ajax({
         type: 'GET',
-        url: 'http://tigertalkapi.herokuapp.com/posts/',
+        url: 'https://tigertalkapi.herokuapp.com/posts/',
         data: { get_param: 'value' },
         dataType: 'json',
         success: function (data) {
 			length = data.length;
             var out = "";
-			var i;
-			for (i = 0; i < data.length; i ++) {
-			out += '<div class="chunk"> <div class="media offset-md-1"> <div class="media-body"> <div class="entry" id="e' + i;
-			out += '">' + data[i].content + '</div> <div class="comments" id="c' + i + '">';
-			comments = data[i].comments;
-			var j;
-			for (j = 0; j < comments.length; j++) {
-				out += '<div class="media mt-1"> <div class="media-body"> <div class="reply">';
-				out += comments[j].content;
-				out += '</div> </div> </div>';
-			}
-			out += '<form class="replying"> <div> <textarea name="entry" cols="100" rows="2" placeholder="Reply"></textarea>';
-			out += '</div><div><button>Post</button></div></form></div>';
-			out += ' </div> </div> </div>';
+			for (let i = data.length-1; i >= 0; i--) {
+				let id = data[i].id;
+				out += '<div class="chunk"> <div class="media offset-md-0"> <div class="media-body"> <div class="entry" id="e' + id;
+				out += '">' + data[i].content + '</div> <div class="comments" id="c' + id + '">';
+				comments = data[i].comments;
+				var j;
+				for (j = 0; j < comments.length; j++) {
+					out += '<div class="media mt-1"> <div class="media-body"> <div class="reply">';
+					out += comments[j].content;
+					out += '</div> </div> </div>';
+				}
+				out += '<form class="replying"> <div> <textarea name="entry" cols="100" rows="2" placeholder="Reply"></textarea>';
+				out += '</div><div><button>Post</button></div></form></div>';
+				out += ' </div> </div> </div>';
 			}
             $('#chunks').append(out);
 
 			// Assign click events to posts to show comments on click
-			for (h = 0; h < length; h++) {
-				console.log(h);
-				let e = "#e" + h;
-				let c = "#c" + h;
-				$(e).click(function() {
-					if ($(c).css("display") === "none") {
-						$(c).css("display", "block");
-					}
-					else {
-						$(c).css("display", "none");
-					}
-				});
+			for (let i = 0; i < data.length; i++) {
+				addShowCommentsEvent(data[i].id);
 			}
         },
         error: function () {
-        	window.alert("rip");
+        	window.alert("Could not get posts");
         }
     });
 
-	// this might be problematic
 	$("#mainpost").click(function(){
-		var text = $("#maintext").val();
+		let text = $("#maintext").val();
+		$("#maintext").val("");
 		if (text.length != 0) {
-			length = length + 1;
 			$.ajax({
 				type: 'POST',
 				url: 'http://tigertalkapi.herokuapp.com/posts/',
@@ -58,30 +47,34 @@ $(document).ready(function() {
 				data: {
 					"content": text
 				},
-				success: function(data) {
-					var toAppend = '<div class="chunk"> <div class="media offset-md-1"> <div class="media-body"> <div class="entry" id="e' + length;
-					toAppend += '">' + data['content'] + '</div> <div class="comments" id="c' + length + '">';
-					toAppend += '<form class="replying"> <div> <textarea name="entry" cols="100" rows="2" placeholder="Reply"></textarea>';
-					toAppend += '</div><div><button>Post</button></div></form></div>';
-					toAppend += ' </div> </div> </div>';
-					$('#chunks').prepend(toAppend);
-
-					h++;
-					let e = "#e" + h;
-					let c = "#c" + h;
-					$("#e" + length).click(function () {
-						if ($("#c" + length).css("display") === "none") {
-							$(c).css("display", "block");
-						}
-						else {
-							$("#c" + length).css("display", "none");
-						}
-					});
-
+				success: function(response){
+					displayPost(text, response['id']);
 				}
 			});
 		}
-
 	});
 
+	function addShowCommentsEvent(id) {
+		let e = "#e" + id;
+		let c = "#c" + id;
+		$(e).click(function() {
+			if ($(c).css("display") === "none") {
+				$(c).css("display", "block");
+			}
+			else {
+				$(c).css("display", "none");
+			}
+		});
+	}
+
+	function displayPost(newPost, id) {
+		var toAppend = '<div class="chunk"> <div class="media offset-md-0"> <div class="media-body"> <div class="entry" id="e' + id;
+		toAppend += '">' + newPost + '</div> <div class="comments" id="c' + id + '">';
+		toAppend += '<form class="replying"> <div> <textarea name="entry" cols="100" rows="2" placeholder="Reply"></textarea>';
+		toAppend += '</div><div><button>Post</button></div></form></div>';
+		toAppend += ' </div> </div> </div>';
+		$('#chunks').prepend(toAppend);
+
+		addShowCommentsEvent(id);
+	}
 });
