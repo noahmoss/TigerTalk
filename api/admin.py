1516763ad888fdb2378c9f55b16d4ff6aff5753a
Noah Moss <noahbmoss@gmail.com>
2018-04-22 02:41:47 -0400

    adjust admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth import get_user_model
-from api.models import Post, Comment
-User = get_user_model()
+from api.models import Post, Comment, User
 
 # Register your models here.
 admin.site.register(Post)
